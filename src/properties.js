fedaa3d460525f5b28f6fe27d8b47aa793c70a04
soare23 <toma.mihai.sorin23@gmail.com>
2021-02-18 19:22:47 +0200

    initial commit

---
+++ src/properties.js
@@ -0,0 +1,7 @@
+export const properties = {
+  firstKey: '1dfec2d9def1413d92b176006307e197',
+  secondKey: 'a1689071996f543429fedccf5f0885331',
+  thirdKey: 'a24ba5878e2014cfe86b7c4144fc1abed',
+  forthKey: '1a1a16a6eed0487a8ee785280b261e5b',
+  fifthKey: '0775f3f53e2b495fa444ecd7a20a0f91',
+};
