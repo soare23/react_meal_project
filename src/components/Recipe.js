dfef3662b4350d295d950e4806cec140324c0505
soare23 <toma.mihai.sorin23@gmail.com>
2021-02-18 19:18:01 +0200

    user can see recipe for selected meal

---
+++ src/components/Recipe.js
@@ -0,0 +1,52 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { properties } from '../properties';
+
+function Recipe(props) {
+  let id = props.match.params.id;
+
+  const [recipe, setRecipe] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(
+        `https://api.spoonacular.com/recipes/${id}/information?apiKey=${properties.forthKey}&includeNutrition=false`
+      )
+      .then((response) => {
+        setRecipe(response.data.extendedIngredients);
+      });
+  }, [id]);
+
+  return (
+    <>
+      <div className="container">
+        <table className="table table-striped table-light">
+          <thead className="table-warning">
+            <tr>
+              <th scope="col">Aisle</th>
+              <th scope="col">Name</th>
+              <th scope="col">Amount</th>
+
+              <th scope="col">Unit</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            {recipe.map((item, index) => {
+              return (
+                <tr key={index}>
+                  <th>{item.aisle}</th>
+                  <td>{item.name}</td>
+                  <td>{item.amount + ' ' + item.measures.metric.unitLong}</td>
+                  <td>{item.unit}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+    </>
+  );
+}
+
+export default Recipe;
